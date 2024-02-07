4464bfa0ba066ce0bb273fec8cfb7f720da12770
Kinga-El <kinga.loose@outlook.com>
2024-02-07 17:46:29 +0100

    create first header component

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+    <header class="Header">
+        <h1 class="Header__title">Todo app</h1>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+.Header {
+  background-color: #046b99;
+  color:white;
+
+  &__title {
+    padding: $gutterExtraLarge; 
+    margin:0; 
+    font-size: 30px;
+  }
+}
+</style>
